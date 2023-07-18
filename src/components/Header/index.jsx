61b4f4391db14d49511f1216cc9f701cbd6e188d
paulomigueljs <paulomiguelblues@gmail.com>
2023-07-17 22:28:08 -0400

    Logo and Switch positioned; Switch styled and functionally and animation finished; Missing Filter be added to the header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,33 @@
-import bgHeaderDesktop from "../../assets/images/desktop/bg-pattern-header.svg";
+import logo from "../../assets/images/desktop/logo.svg";
 import iconSun from "../../assets/images/desktop/icon-sun.svg";
 import iconMoon from "../../assets/images/desktop/icon-moon.svg";
 
-
-
-import { HeaderContainer, Container } from "./styles";
+import { HeaderContainer, Switch, Container } from "./styles";
+import { useState } from "react";
 
 export default function Header() {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleToggle = () => setIsToggled(!isToggled);
+
   return (
     <HeaderContainer>
       <Container>
         <a href="/">
-          <img src={bgHeaderDesktop} alt="Devjobs Logo" />
+          <img src={logo} alt="Devjobs Logo" />
         </a>
-        
+
         <div>
           <img src={iconSun} alt="Sun Light Theme" />
-          <button>Togle</button>
+
+          <Switch
+        
+            onToggle={handleToggle}
+          >
+            <input type="checkbox" checked={isToggled} onChange={handleToggle} />
+            <span className="slider round"></span>
+          </Switch>
+
           <img src={iconMoon} alt="Moon Dark Theme" />
         </div>
       </Container>
